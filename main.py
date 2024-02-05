0802cfdc9773fad9817900512eb50a2ebda9003c
Andris <buzasandrascsaba@gmail.com>
2024-02-05 19:51:47 +0100

    third commit

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print('git trippin dawg')
+import gspread
+from pprint import pprint
+
+gc = gspread.service_account(filename='creds.json',
+                             scopes=gspread.auth.DEFAULT_SCOPES)
+spreadsheet = gc.open('astronauts')
+worksheet = spreadsheet.worksheet('astronauts')
+
+def main():
+    sheet_data = worksheet.get_all_values()
