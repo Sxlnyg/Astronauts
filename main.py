d59d46cf08844c8f8831f3b8b8cc33cc829c3fe0
Andris <buzasandrascsaba@gmail.com>
2024-02-05 20:02:51 +0100

    commit

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import gspread
-from pprint import pprint
+
+from collections import Counter
 
 gc = gspread.service_account(filename='creds.json',
                              scopes=gspread.auth.DEFAULT_SCOPES)
 spreadsheet = gc.open('astronauts')
 worksheet = spreadsheet.worksheet('astronauts')
 
+
 def main():
     sheet_data = worksheet.get_all_values()
+    birth_months = [int(entry[4].split('/')[0]) for entry in sheet_data[1:]]
+    month_counts = Counter(birth_months)
+    sorted_months = sorted(month_counts.items(), key=lambda x: x[1], reverse=True)
+    top_three_months = sorted_months[:3]
+    total_births = sum(month_counts.values())
+    print("A három leggyakoribb születési hónap az űrhajósok között:")
+    for month, count in top_three_months:
+        percentage = (count / total_births) * 100
+        print(f"{month}. hónap: {percentage:.1f}%")
+
+main()
